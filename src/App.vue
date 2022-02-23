66d8fe5bebae7c9d36841a7b25f7117038a77130
zp <763360229@qq.com>
2022-02-23 17:24:22 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header/>
+    <!-- //路由组件出口的地方 -->
+
+    <router-view></router-view>
+    <!-- 在home、Search显示的，在登录、注册隐藏 -->
+    <Footer v-show="$route.meta.show" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name:'',
+  components:{
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//引入路由
+import router from '@/router';
+//三级联动组件---全局组件
+import TypeNav from '@/pages/Home/TypeNav';
+//第一个参数：全局组件的名字  第二个参数：哪一个组件
+Vue.component(TypeNav.name,TypeNav)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //注册路由：底下的写法
+  //注册路由信息：当这里书写router的时候，组件身上都拥有$router，$router属性
+  router,
 }).$mount('#app')
